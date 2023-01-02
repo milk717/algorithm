9efcc224552084deda27199b7b55a9b3f872e397
milk717 <robolindasoo@gmail.com>
2023-01-02 22:54:21 +0900

    프로그래머스_코딩테스트입문_직각삼각형 출력하기

--- programmers/코딩테스트입문_JavaScript/최빈값 구하기.js
+++ programmers/코딩테스트입문_JavaScript/최빈값 구하기.js
@@ -1,3 +1,8 @@
+/*
+최빈값의 개수가 아니라 최빈값이 무엇인지 return해야한다.
+문제를 꼼꼼하게 읽지 않아서 시간을 오래 사용했다.
+ */
+
 function solution(array) {
     let maxValue = Math.max(...array);
     let checkArr = new Array(maxValue+1).fill(0)
