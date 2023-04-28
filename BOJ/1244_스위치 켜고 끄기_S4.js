22dfa6791f4364f897486dca91896498b2b02ac2
milk717 <robolindasoo@gmail.com>
2023-04-28 23:57:42 +0900

    구현

---
+++ BOJ/1244_스위치 켜고 끄기_S4.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? '/dev/stdin' : '../input.txt';
+let [n,switchList, m, ...peopleList] = fs.readFileSync(filePath).toString().trim().split('\n');
+switchList = switchList.split(' ').map(v => +v);
+peopleList = peopleList.map(v=>v.split(' ').map(p =>+p))
+
+for(let i = 0; i<m; i++){
+    let [sex, num] = peopleList[i];
+    if(sex === 2){
+        num--;
+        switchList[num] = +!switchList[num];
+        for(let j = 1; j<n/2; j++){
+            if(num-j >= 0 && num+j < n){
+                if(switchList[num-j] === switchList[num+j]){
+                    switchList[num-j] = +!switchList[num-j];
+                    switchList[num + j] = +!switchList[num + j];
+                }else{
+                    break;
+                }
+            }
+        }
+    }else{
+        for (let j = 1; j * num < n; j++) {
+            switchList[num*j - 1] = +!switchList[num*j - 1];
+            console.log(num*j - 1);
+        }
+    }
+}
+
+let res = '';
+
+switchList.forEach((v,i)=>{
+    res += v;
+    if(i && i%20 === 0){
+        res += '\n';
+    }else{
+        res += ' ';
+    }
+})
+
+console.log(res);
