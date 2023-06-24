09a70f02455d08e5c69436db634038caea177aad
milk717 <robolindasoo@gmail.com>
2023-06-25 00:55:37 +0900

    Bronze 문제 이동

--- temp.js
+++ temp.js
@@ -1,11 +1,1 @@
-function solution(skill, skill_trees) {
-    skill_trees.forEach(tree=>{
-        const pattern = `\\w*${skill[0]}\\w*${skill[1]}\\w*${skill[2]}\w*`;
-        const regex = new RegExp(pattern,'g');
-        console.log(tree.split(regex));
-    })
-}
 
-skill = "CBD";
-arr = ["BACDE", "CBADF", "AECB", "BDA"];
-solution(skill,arr);
