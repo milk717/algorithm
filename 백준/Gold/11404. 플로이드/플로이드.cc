9a11bfd870b0b47032c49a12236b0ea19832d67d
Rubato <57657868+milk717@users.noreply.github.com>
2025-10-07 15:12:43 +0900

    [Gold IV] Title: 플로이드, Time: 76 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,83 +1,47 @@
 #include <iostream>
-
 #include <algorithm>
-
 #define INF 210000000
-
 #define MAX 100
 
 using namespace std;
 
-int arr[100][100];
-
 int sho[100][100];
 
 int main(void){
-
-    fill(&arr[0][0], &arr[MAX][MAX], INF);
-
     fill(&sho[0][0], &sho[MAX][MAX], INF);
-
     int n, m;
-
     cin >> n;
-
     cin >> m;
 
-    for(int i =0; i<m; i++){
-
-        int a,b,c;
-
-        cin >>a>>b>>c;
-
+    for(int i = 0; i < m; i++){
+        int a, b, c;
+        cin >> a >> b >> c;
         a--;
-
         b--;
-
-        arr[a][b] =min(arr[a][b],c);
-
-        sho[a][b]=arr[a][b];
-
+        sho[a][b] = min(sho[a][b], c);
     }
 
-    for(int k =0; k<n;k++){
-
-        sho[k][k]=0;
-
+    for(int k = 0; k < n; k++){
+        sho[k][k] = 0;
     }
 
-    for(int k =0; k<n;k++){
-
-        for(int i =0; i<n;i++){
-
-            for(int j =0; j<n;j++){
-
-                if(i==j)continue;
-
-                sho[i][j]=min(sho[i][j],sho[i][k]+sho[k][j]);
-
+    for(int k = 0; k < n; k++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if (i == j) continue;
+                sho[i][j] = min(sho[i][j], sho[i][k]+sho[k][j]);
             }
-
         }
-
     }
 
-    for(int i=0; i<n;i++){
-
-        for(int j =0; j<n;j++){
-
-            if(sho[i][j]==INF){
-
-                sho[i][j]=0;
-
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(sho[i][j] == INF){
+                sho[i][j] = 0;
             }
-
-            cout<<sho[i][j]<<" ";
-
+            cout << sho[i][j] << " ";
         }
-
-        if(i!=n-1) cout<<"\n";
-
+        if(i != n-1) cout << "\n";
     }
 
 }
