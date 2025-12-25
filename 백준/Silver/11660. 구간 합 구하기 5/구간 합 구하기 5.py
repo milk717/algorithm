2b9280f1487369d4126dae41e6d4c145c94aeda8
Rubato <57657868+milk717@users.noreply.github.com>
2025-12-25 23:28:21 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 312 ms, Memory: 149080 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,7 +1,20 @@
+"""
+1 2 3 4
+2 3 4 5
+3 4 5 6
+4 5 6 7
+
+(1, 1): 위에서 하나 왼쪽에서 하나, 대각선 빼기
+
+1 3 6 10
+3 8 15 24
+6 15 27 42
+10 24 42 64
+"""
+
 import sys
 
 input = sys.stdin.readline
-print = sys.stdout.write
 
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
@@ -10,15 +23,21 @@ range_case = [tuple(map(lambda x: int(x) - 1, input().split())) for _ in range(m
 prefix_sum_arr = [arr[i].copy() for i in range(n)]
 
 for i in range(n):
-    for j in range(1, n):
-        prefix_sum_arr[i][j] = prefix_sum_arr[i][j - 1] + arr[i][j]
+    for j in range(n):
+        prefix_sum_arr[i][j] = (
+            (prefix_sum_arr[i - 1][j] if i - 1 >= 0 else 0)
+            + (prefix_sum_arr[i][j - 1] if j - 1 >= 0 else 0)
+            + arr[i][j]
+            - (prefix_sum_arr[i - 1][j - 1] if i - 1 >= 0 and j - 1 >= 0 else 0)
+        )
 
-res_arr = []
+res = []
 for x1, y1, x2, y2 in range_case:
-    res = 0
-    for x in range(x1, x2 + 1):
-        res += prefix_sum_arr[x][y2] - (prefix_sum_arr[x][y1 - 1] if y1 - 1 >= 0 else 0)
+    res.append(
+        prefix_sum_arr[x2][y2]
+        - (prefix_sum_arr[x2][y1 - 1] if y1 - 1 >= 0 else 0)
+        - (prefix_sum_arr[x1 - 1][y2] if x1 - 1 >= 0 else 0)
+        + (prefix_sum_arr[x1 - 1][y1 - 1] if x1 - 1 >= 0 and y1 - 1 >= 0 else 0)
+    )
 
-    res_arr.append(res)
-
-print("\n".join(map(str, res_arr)))
+print("\n".join(map(str, res)))
