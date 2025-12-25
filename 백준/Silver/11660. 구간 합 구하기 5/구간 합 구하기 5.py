9125fd67feb6f86655b1f92f0ce9ce1133d86ee3
Rubato <57657868+milk717@users.noreply.github.com>
2025-12-25 20:09:54 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1524 ms, Memory: 143284 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+range_case = [tuple(map(lambda x: int(x) - 1, input().split())) for _ in range(m)]
+
+prefix_sum_arr = [arr[i].copy() for i in range(n)]
+
+for i in range(n):
+    for j in range(1, n):
+        prefix_sum_arr[i][j] = prefix_sum_arr[i][j - 1] + arr[i][j]
+
+for x1, y1, x2, y2 in range_case:
+    res = 0
+    for x in range(x1, x2 + 1):
+        res += prefix_sum_arr[x][y2] - (prefix_sum_arr[x][y1 - 1] if y1 - 1 >= 0 else 0)
+
+    print(res)
