8e6b45a40591a2e915e305c950c759582e39e734
Rubato <57657868+milk717@users.noreply.github.com>
2026-03-17 16:29:54 +0900

    [Gold V] Title: Z, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -4,27 +4,26 @@ input = sys.stdin.readline
 
 N, R, C = map(int, input().split())
 
-def main(n, acc, r, c, cnt):
+def main(n, acc, r, c):
     # print(n, acc, r, c, cnt)
-    if cnt == N:
-        print(acc)
-        return
+    if n == 0:
+        return acc
     
     # 0
     if r < 2**(n-1) and c < 2**(n-1):
         # print(0)
-        main(n-1, acc, r, c, cnt + 1)
+        return main(n-1, acc, r, c)
     # 1
     elif r < 2**(n-1) and c >= 2**(n-1):
         # print(1)
-        main(n-1, acc + 2**(2*(n-1)), r, c - (2**(n-1)), cnt+1)
+        return main(n-1, acc + 2**(2*(n-1)), r, c - (2**(n-1)))
     # 2
     elif r >= 2**(n-1) and c < 2**(n-1):
         # print(2)
-        main(n-1, acc + 2 * (2**(2*(n-1))), r - (2**(n-1)), c, cnt+1)
+        return main(n-1, acc + 2 * (2**(2*(n-1))), r - (2**(n-1)), c)
     # 3
     elif r >= 2**(n-1) and c >= 2**(n-1):
         # print(3)
-        main(n-1, acc + 3 * (2**(2*(n-1))), r - (2**(n-1)), c - (2**(n-1)), cnt+1)
+        return main(n-1, acc + 3 * (2**(2*(n-1))), r - (2**(n-1)), c - (2**(n-1)))
 
-main(N, 0, R, C, 0)
+print(main(N, 0, R, C))
